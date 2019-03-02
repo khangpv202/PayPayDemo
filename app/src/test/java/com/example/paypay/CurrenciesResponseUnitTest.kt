981f3e34d0076aec745ef59519bb53cd51eea868
khangpv2 <khangpv202@gmail.com>
2019-03-02 23:27:17 +0700

    refacter test

--- app/src/test/java/com/example/paypay/CurrenciesResponseUnitTest.kt
+++ app/src/test/java/com/example/paypay/CurrenciesResponseUnitTest.kt
@@ -18,7 +18,7 @@ import java.math.BigDecimal
  */
 class CurrenciesResponseUnitTest {
     @Test
-    fun addition_isCorrect() {
+    fun test_custome_RateItemTypeAdapter() {
         val input = "{\n" +
                 "  \"success\":true,\n" +
                 "  \"terms\":\"https:\\/\\/currencylayer.com\\/terms\",\n" +
